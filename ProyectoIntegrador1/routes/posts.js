eebeeb9f49177aa84a8f9297e4d9b849a5350dba
MartinGutman5 <63519543+MartinGutman5@users.noreply.github.com>
2021-09-10 12:41:44 -0400

    prolijidad

--- ProyectoIntegrador1/routes/posts.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const postsController = require('../controllers/postsController');
-router.get('/detail', postsController.posts);
-router.get('/add', postsController.agregarPost );
-
-module.exports = router
