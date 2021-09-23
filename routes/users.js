2d511c311c23c35879bd66e4c8ee2668e121e3a1
martumoroni <80982224+martumoroni@users.noreply.github.com>
2021-09-23 12:22:57 -0300

    Corrección ruta miPerfil

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersController');
 /* GET users listing. */
-router.get('/profile/:id', usersController.miPerfil);
+router.get('/profile/1', usersController.miPerfil);
 router.get('/register', usersController.registracion);
 router.get ('/login', usersController.login) 
 router.get ('/edit', usersController.editarPerfil)
