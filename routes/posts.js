66965956ac6a03a29ced79cddf4ae0c36cc7c0a8
MartinGutman5 <63519543+MartinGutman5@users.noreply.github.com>
2021-09-24 14:13:47 -0300

    aaa

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const postsController = require('../controllers/postsController');
-router.get('/detail/:id', postsController.posts);
+router.get('/detail/:id', postsController.post);
 router.get('/add', postsController.agregarPost );
 
 module.exports = router
